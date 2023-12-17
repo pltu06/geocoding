6087f84ec2e9a0af900856dfc13668dc4ca1209f
Frank Tu <tfsheng@imac-tu-family.lan>
2023-12-17 14:35:12 -0600

    Added SVI tract data to geocoding function

--- src/svi_adi_function.R
+++ src/svi_adi_function.R
@@ -9,11 +9,25 @@ svi_adi_function <- function(data = x, adi_year = 2021){
   library(tidyverse)
   library(readr)
   
-  svi_url <- "https://raw.githubusercontent.com/pltu06/geocoding/main/Data/svi_il_bg_2020.csv"
+  svi_url_bg <- "https://raw.githubusercontent.com/pltu06/geocoding/main/Data/svi_il_bg_2020.csv"
+  
+  svi_data_bg <- read_csv(url(svi_url_bg))%>%
+    select(svi_block = THEMES, GEOID)%>%
+    mutate(FIPS = as.numeric(GEOID))
+  
+  svi_url <- "https://raw.githubusercontent.com/pltu06/geocoding/main/Data/svi_il_2020.csv"
   
   svi_data <- read_csv(url(svi_url))%>%
-    select(svi = THEMES, GEOID)%>%
-    mutate(FIPS = as.numeric(GEOID))
+    select(LOCATION, svi_tract = RPL_THEMES, FIPS)%>%
+    separate(
+      LOCATION, 
+      into = c("census_tract", "county", "state"), 
+      sep = ",", 
+      remove = FALSE
+    ) %>%
+    mutate(tract = as.factor(gsub("[^0-9.]", "", LOCATION)), 
+           county_name = sub("\\s+", "", county))%>%
+    select(-county,-state)
   
   adi_url <- paste0("https://raw.githubusercontent.com/pltu06/geocoding/main/Data/adi_il_", adi_year, ".csv")
   
@@ -29,18 +43,23 @@ svi_adi_function <- function(data = x, adi_year = 2021){
            county = formatC(cxy_county_id, width = 3, flag = "0"), 
            tract = formatC(cxy_tract_id, width = 6, flag = "0"),
            block_group = substr(cxy_block_id, 1, 1),
-           geoid = as.numeric(paste0(state, county, tract, block_group)))
+           geoid_block = as.numeric(paste0(state, county, tract, block_group)),
+           geoid_tract = as.numeric(paste0(state, county, tract)))
   
   svi_tract <- left_join(census_tracts, svi_data,
-                         by = c("geoid" = "FIPS"))%>%
-    left_join(., adi_data, by = c("geoid" = "FIPS"))
+                         by = c("geoid_tract" = "FIPS"))%>%
+    left_join(., svi_data_bg, by = c("geoid_block" = "FIPS"))%>%
+    left_join(., adi_data, by = c("geoid_block" = "FIPS"))
   
-  data$svi <- svi_tract$svi
+  data$svi_tract <- svi_tract$svi_tract
+  data$svi_block <- svi_tract$svi_block
   data$lon <- svi_tract$cxy_lon
   data$lat <- svi_tract$cxy_lat
-  data$geoid <- svi_tract$geoid
+  data$geoid_tract <- svi_tract$geoid_tract
+  data$geoid_block <- svi_tract$geoid_block
   data$adi_national <- svi_tract$ADI_NATRANK
   data$adi_state <- svi_tract$ADI_STATERNK
+  data$tract <- svi_tract$tract.y
   
   return(data)
   
